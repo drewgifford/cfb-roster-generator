aab3a2feaae32a13fe86792b4c1a94f4276f3974
drewgifford <drew@drewgifford.com>
2025-12-29 17:48:20 -0500

    fix: Remove base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const production = process.env.NODE_ENV === 'production';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -14,10 +12,6 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-
-		paths: {
-			base: production ? '/cfb-roster-generator' : ''
-		}
 	}
 };
 
